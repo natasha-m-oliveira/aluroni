1b9544a6799c736eeae6a279041c0649ea143871
Natasha de Matos Oliveira <natashamo@uninove.br>
2022-08-16 19:02:25 -0700

    Page NotFound

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import Cardapio from "pages/Cardapio";
 import Inicio from "pages/Inicio";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sobre from "pages/Sobre";
+import Footer from "components/Footer";
+import NotFound from "pages/NotFound";
 
 export default function AppRouter() {
   return (
@@ -17,8 +19,10 @@ export default function AppRouter() {
             <Route index element={<Inicio />} />
             <Route path="cardapio" element={<Cardapio />} />
             <Route path="sobre" element={<Sobre />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
